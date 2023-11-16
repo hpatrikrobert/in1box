e36a9976398d45fd42ffa133d4aa6cd801ff30a6
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-16 12:30:26 +0100

    Add error handling to db connection

--- libs/db.js
+++ libs/db.js
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
-const connectDB = () => {
-    try {
-        mongoose.connect(process.env.MONGODB_URI);
-    } catch (error) {
-        console.log(error);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {});
+
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Disconnected from MongoDB");
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
 };
 
 export default connectDB;
